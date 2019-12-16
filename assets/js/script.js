39f659959526ed97b75af763f7602b9b6bd19f84
Madeline Jimenez <madelineijimenez@gmail.com>
2019-12-15 21:00:35 -0500

    Display UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var APIKey = "f954ed6275dcbf562c6fb2740dba1913";
 var cityName = "";
 var searchedCities = [];
+var lat = "";
+var lon = "";
 
 
 // This function handles events where the search button is clicked
@@ -17,69 +19,11 @@ $(".search-button").on("click", function(event) {
 
     searchedCities.push(cityName);
 
-    // Here we are building the URL we need to query the database for current weather.
-    var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
-    // Here we are building the URL we need to query the database for 5 day forcast.
-    var queryURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
-
-    // Here we run our AJAX call to the OpenWeatherMap API for current forcast.
-    $.ajax({
-        url: queryURLWeather,
-        method: "GET"
-        })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function(response) {
-            // console.log(queryURLWeather);
-            // console.log(response);
-
-            // Display current weather results
-            $("#currentWeather > .name").text(response.name);
-            $("#currentWeather > .date").text(moment().format("dddd, MMMM Do YYYY"));
-            $("#currentWeather > .icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
-            $("#currentWeather > .temperature").text("Temperature: " + response.main.temp + String.fromCharCode(176) + "F");
-            $("#currentWeather > .humidity").text("Humidity: " + response.main.humidity);
-            $("#currentWeather > .wind-speed").text("Wind Speed: " + response.wind.speed);
-        });
-
-    // Here we run our AJAX call to the OpenWeatherMap API for daily forecast.
-    $.ajax({
-        url: queryURLFiveDay,
-        method: "GET"
-        })
-        .then(function(response) {
-            // console.log(response);
-            // Display 5 day forecast container
-            $("#dailyForecast").attr("style", "display:block;");
-
-            // Display current weather results
-            //HELP: How can I loop this and display dynamically?
-            $("#dailyForecastCards > #day1 .date").text(response.list[1].dt_txt);
-            $("#dailyForecastCards > #day1 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[1].weather[0].icon + "@2x.png");
-            $("#dailyForecastCards > #day1 .temperature").text("Temperature: " + response.list[1].main.temp + String.fromCharCode(176) + "F");
-            $("#dailyForecastCards > #day1 .humidity").text("Humidity: " + response.list[1].main.humidity);
-
-            $("#dailyForecastCards > #day2 .date").text(response.list[2].dt_txt);
-            $("#dailyForecastCards > #day2 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon + "@2x.png");
-            $("#dailyForecastCards > #day2 .temperature").text("Temperature: " + response.list[2].main.temp + String.fromCharCode(176) + "F");
-            $("#dailyForecastCards > #day2 .humidity").text("Humidity: " + response.list[2].main.humidity);
-
-            $("#dailyForecastCards > #day3 .date").text(response.list[3].dt_txt);
-            $("#dailyForecastCards > #day3 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[3].weather[0].icon + "@2x.png");
-            $("#dailyForecastCards > #day3 .temperature").text("Temperature: " + response.list[3].main.temp + String.fromCharCode(176) + "F");
-            $("#dailyForecastCards > #day3 .humidity").text("Humidity: " + response.list[3].main.humidity);
-
-            $("#dailyForecastCards > #day4 .date").text(response.list[4].dt_txt);
-            $("#dailyForecastCards > #day4 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[4].weather[0].icon + "@2x.png");
-            $("#dailyForecastCards > #day4 .temperature").text("Temperature: " + response.list[4].main.temp + String.fromCharCode(176) + "F");
-            $("#dailyForecastCards > #day4 .humidity").text("Humidity: " + response.list[4].main.humidity);
-
-            $("#dailyForecastCards > #day5 .date").text(response.list[5].dt_txt);
-            $("#dailyForecastCards > #day5 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[5].weather[0].icon + "@2x.png");
-            $("#dailyForecastCards > #day5 .temperature").text("Temperature: " + response.list[5].main.temp + String.fromCharCode(176) + "F");
-            $("#dailyForecastCards > #day5 .humidity").text("Humidity: " + response.list[5].main.humidity);
-        });
-
-    // Calling the renderButtons function to display the initial list of cities
+    // Display today's weather
+    renderCurrentWeather();
+    // Display 5 day forecast
+    renderDailyWeather();
+    // Display the initial list of cities
     renderCities();
 });
 
@@ -98,7 +42,112 @@ function renderCities() {
         newCity.attr("href", "#");
         $("#cityHistory").append(newCity);
     }
-}
+};
+
+
+
+function renderCurrentWeather() {
+        // Here we are building the URL we need to query the database for current weather.
+        var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
+
+    // Here we run our AJAX call to the OpenWeatherMap API for current forcast.
+    $.ajax({
+        url: queryURLWeather,
+        method: "GET"
+    })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+        // console.log(queryURLWeather);
+        console.log(response);
+
+        // Display current weather results
+        $("#currentWeather > .name").text(response.name);
+        $("#currentWeather > .date").text(moment().format("dddd, MMMM Do YYYY"));
+        $("#currentWeather > .icon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+        $("#currentWeather > .temperature").text("Temperature: " + response.main.temp + String.fromCharCode(176) + "F");
+        $("#currentWeather > .humidity").text("Humidity: " + response.main.humidity);
+        $("#currentWeather > .wind-speed").text("Wind Speed: " + response.wind.speed);
+
+        // Store longitude and latiture in global variables
+        var cityLon = response.coord.lon;
+        var cityLat = response.coord.lat;
+        lon = cityLon;
+        lat = cityLat;
+
+        // Display UV index
+        renderUVindex();
+    });
+};
+
+
+
+function renderUVindex() {
+    // Here we are building the URL we need to query the database for current weather.
+    var queryURLuvIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURLuvIndex,
+        method: "GET"
+    })
+
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+        // console.log(queryURLuvIndex);
+        // console.log(response);
+
+        // Store UX Index in global variable
+        var UVindex = response.value;
+        $("#currentWeather > .uv-index").text("UV Index: " + UVindex);
+    });
+};
+
+
+
+function renderDailyWeather() {
+    // Here we are building the URL we need to query the database for 5 day forcast.
+    var queryURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + APIKey;
+
+    // Here we run our AJAX call to the OpenWeatherMap API for daily forecast.
+    $.ajax({
+        url: queryURLFiveDay,
+        method: "GET"
+    })
+    .then(function(response) {
+        // console.log(response);
+
+            // Display 5 day forecast container
+            $("#dailyForecast").attr("style", "display:block;");
+        
+            // Display current weather results
+            //HELP: How can I loop this and display dynamically?
+            $("#dailyForecastCards > #day1 .date").text(response.list[0].dt_txt);
+            $("#dailyForecastCards > #day1 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
+            $("#dailyForecastCards > #day1 .temperature").text("Temperature: " + response.list[0].main.temp + String.fromCharCode(176) + "F");
+            $("#dailyForecastCards > #day1 .humidity").text("Humidity: " + response.list[0].main.humidity);
+        
+            $("#dailyForecastCards > #day2 .date").text(response.list[8].dt_txt);
+            $("#dailyForecastCards > #day2 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png");
+            $("#dailyForecastCards > #day2 .temperature").text("Temperature: " + response.list[8].main.temp + String.fromCharCode(176) + "F");
+            $("#dailyForecastCards > #day2 .humidity").text("Humidity: " + response.list[8].main.humidity);
+        
+            $("#dailyForecastCards > #day3 .date").text(response.list[16].dt_txt);
+            $("#dailyForecastCards > #day3 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png");
+            $("#dailyForecastCards > #day3 .temperature").text("Temperature: " + response.list[16].main.temp + String.fromCharCode(176) + "F");
+            $("#dailyForecastCards > #day3 .humidity").text("Humidity: " + response.list[16].main.humidity);
+        
+            $("#dailyForecastCards > #day4 .date").text(response.list[24].dt_txt);
+            $("#dailyForecastCards > #day4 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + "@2x.png");
+            $("#dailyForecastCards > #day4 .temperature").text("Temperature: " + response.list[24].main.temp + String.fromCharCode(176) + "F");
+            $("#dailyForecastCards > #day4 .humidity").text("Humidity: " + response.list[24].main.humidity);
+        
+            $("#dailyForecastCards > #day5 .date").text(response.list[32].dt_txt);
+            $("#dailyForecastCards > #day5 .icon").attr("src", "http://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + "@2x.png");
+            $("#dailyForecastCards > #day5 .temperature").text("Temperature: " + response.list[32].main.temp + String.fromCharCode(176) + "F");
+            $("#dailyForecastCards > #day5 .humidity").text("Humidity: " + response.list[32].main.humidity);
+    });
+};
+
+
 
 
 
